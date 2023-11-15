65360e975cb3a12be44ac571466d2b9d335770a1
JuanAlvarez98 <juan98.roque@gmail.com>
2023-11-15 12:38:39 -0600

    cambios

--- IMPINFORMATICO/resources/views/modseguridad/perfil.blade.php
+++ IMPINFORMATICO/resources/views/modseguridad/perfil.blade.php
@@ -1,60 +1,75 @@
+@extends('layouts.app')  
 @extends('adminlte::page')
-
 @section('title', 'Perfil')
 
 @section('content_header')
-    <h1><b>Editar Perfil</h1></b>
+   
 @stop
 
 @php
-    $usuario = session('credenciales');  
+    $usuario = session('credenciales'); 
 @endphp
 
 
-
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center align-items-up" style="height: 100vh;">
-            <div class="col-md-6">
-                <div class="card shadow-lg"> 
-                    <div class="card-header bg-primary text-white"> <!-- Cambiamos el fondo y color del encabezado -->
-                        <h3 class="card-title">Información de Usuario</h3>
-                    </div>
-                    <div class="card-body">
-                        <form action="{{ route('ContraPerfil.index') }}" method="get">
-                            @csrf
-                            @if (!empty($usuario))
-                                <div class="form-group">
-                                    <label for="nombre_usuario">Nombre de Usuario</label>
-                                    <input type="text" class="form-control" id="nombre_usuario" name="nombre_usuario" value="{{ $usuario['NOM_USUARIO'] }}">
-                                </div>
-                                <div class="form-group">
-                                    <label for="rol">Rol</label>
-                                    <input type="text" class="form-control" id="rol" name="rol"  value="{{ $usuario['NOM_ROL'] }}">
-                                </div>
-                                <div class="form-group">
-                                    <label for="rol">Correo</label>
-                                    <input type="text" class="form-control" id="corre" name="correo"  value="{{ $usuario['EMAIL'] }}">
-                                </div>
-                            @else
-                                <p>No se ha encontrado un nombre de usuario.</p>
-                            @endif
-                            <div class="form-group">
-                                <label for="contrasena">Cambiar contraseña</label>
-                                <input type="password" class="form-control" id="contrasena" name="CONTRASENA" placeholder="Ingrese su contraseña">
-                            </div>
-                            <div class="d-flex justify-content-between">
-                                <button type="submit" class="btn btn-primary"><b>GUARDAR</b></button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <!-- Contenido de sla vista -->
+    <!-- ... -->
+    <div class="page-content page-container" id="page-content">
+    <div class="padding">
+        <div class="row container d-flex justify-content-center">
+<div class="col-xl-6 col-md-12">
+                                                <div class="card user-card-full">
+                                                    <div class="row m-l-0 m-r-0">
+                                                        <div class="col-sm-4 bg-c-lite-green user-profile">
+                                                            <div class="card-block text-center text-white">
+                                                                <div class="m-b-25">
+                                                                <img width="68" height="68s" src="https://img.icons8.com/glyph-neue/64/group--v1.png" alt="group--v1"/>
+                                                                </div>
+                                                                <h6 class="f-w-600" >{{ $usuario['NOM_USUARIO'] }}</h6>
+                                                                <p>{{ $usuario['NOM_ROL'] }}</p>
+                                                                <i class=" mdi mdi-square-edit-outline feather icon-edit m-t-10 f-16"></i>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-sm-8">
+                                                            <div class="card-block">
+                                                                <h6 class="m-b-20 p-b-5 b-b-default f-w-600">Información</h6>
+                                                                <div class="row">
+                                                                    <div class="col-sm-6">
+                                                                        <p class="m-b-10 f-w-600">Email</p>
+                                                                        <h6 class="text-muted f-w-400">{{ $usuario['EMAIL'] }}</h6>
+                                                                    </div>
+                                                                    <div class="col-sm-6">
+                                                                        <p class="m-b-10 f-w-600">Teléfono</p>
+                                                                        <h6 class="text-muted f-w-400">88586584</h6>
+                                                                    </div>
+                                                                </div>
+                                                                <h6 class="m-b-20 m-t-40 p-b-5 b-b-default f-w-600">Projects</h6>
+                                                                <div class="row">
+                                                                    <div class="col-sm-6">
+                                                                        <p class="m-b-10 f-w-600">Recent</p>
+                                                                        <h6 class="text-muted f-w-400">En mantenimiento</h6>
+                                                                    </div>
+                                                                    <div class="col-sm-6">
+                                                                        <p class="m-b-10 f-w-600">Most Viewed</p>
+                                                                        <h6 class="text-muted f-w-400">En mantenimiento</h6>
+                                                                    </div>
+                                                                </div>
+                                                                <ul class="social-link list-unstyled m-t-40 m-b-10">
+                                                                    <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="facebook" data-abc="true"><i class="mdi mdi-facebook feather icon-facebook facebook" aria-hidden="true"></i></a></li>
+                                                                    <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="twitter" data-abc="true"><i class="mdi mdi-twitter feather icon-twitter twitter" aria-hidden="true"></i></a></li>
+                                                                    <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="instagram" data-abc="true"><i class="mdi mdi-instagram feather icon-instagram instagram" aria-hidden="true"></i></a></li>
+                                                                </ul>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                             </div>
+                                                </div>
+                                            </div>
 @stop
-
 
 @section('footer')
-    <!-- Contenido del pie de página, si es necesario -->
+    <!-- Contenido del pie de página -->
 @stop
+
