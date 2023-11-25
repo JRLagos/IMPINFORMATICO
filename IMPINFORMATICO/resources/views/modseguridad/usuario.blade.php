27ec819a41a2d36b58a576f277b3a3dec5171d45
JRLagos <jrlagosc@unah.hn>
2023-11-25 16:48:40 -0600

    A

--- IMPINFORMATICO/resources/views/modseguridad/usuario.blade.php
+++ IMPINFORMATICO/resources/views/modseguridad/usuario.blade.php
@@ -57,13 +57,6 @@
 
     <div class="d-grid gap-2 d-md-flex justify-content-between align-items-center">
     <h1><b>Usuarios</b></h1>
-    @php
-        $permisoEditar = tienePermiso($permisosFiltrados, 'PER_INSERTAR');
-    @endphp
-    <button class="btn @if (!$permisoEditar) btn-secondary disabled @else btn-success active text-light @endif btn-lg"  
-            data-bs-target="{{ route('ModuloSeguridad.enviar') }}" type="button">
-        <b>Nuevo Usuario</b>
-    </button>
 </div>
 
     @if(session('success'))
